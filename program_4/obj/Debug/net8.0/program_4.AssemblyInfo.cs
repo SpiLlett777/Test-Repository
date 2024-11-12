3edfe141c13b82caa3fa0f0fa4f6bead2d286525
MarkMed777 <nver.karapetyan.2005@list.ru>
2024-11-12 20:33:22 +0300

    Была реализована возможность расчета расходов топлива подводной лодки

--- program_4/obj/Debug/net8.0/program_4.AssemblyInfo.cs
+++ program_4/obj/Debug/net8.0/program_4.AssemblyInfo.cs
@@ -14,13 +14,10 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586bc2cc678db6477a5121a8f246dbe06b7dfa37")]  // Оставляем актуальную версию
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4d7699b8401cc4775c2caeadbffcbccd176c84")]
->>>>>>> 396799a (Была разработана расширенная функция расчета безопасной глубины по температуре воды)
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8402e150b44be01d99bc6c739f7a17f1669ab05")]
 [assembly: System.Reflection.AssemblyProductAttribute("program_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
+
